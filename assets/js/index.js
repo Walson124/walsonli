624c24011daaf41019c4d874eb17ed356b84e5f7
Walson Li <walsonli@Walsons-MacBook-Air.local>
2023-12-03 02:25:45 -0500

    added basic front page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,22 @@
 function openTOC() {
-    console.log("toc button touched");
     const toc = document.getElementById("TOC");
-    toc.style.opacity = 1;
+    const toc_arrow = document.getElementById("TOC-selector-arrow");
+    const toc_label = document.getElementById("TOC-selector-label");
+    toc_arrow.style.opacity = 0;
+    toc_label.style.opacity = 0;
+    toc.style.visibility = 'visible';
     toc.addEventListener('mouseleave', function() {
         // console.log('mouse left');
-        toc.style.opacity = 0;
+        toc.style.visibility = 'hidden';
     })
+}
+
+function toggleTOCHelpers() {
+    // console.log("adding opacity");
+    const toc_arrow = document.getElementById("TOC-selector-arrow");
+    const toc_label = document.getElementById("TOC-selector-label");
+    if (toc_arrow.style.opacity == '0' && toc_label.style.opacity == '0') {
+        toc_arrow.style.opacity = 0.5;
+        toc_label.style.opacity = 0.5;
+    }
 }
